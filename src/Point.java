27f43742451ddd8ef6e66d319abd1ec963471b3d
Daniel Håkansson <daniel.hakansson@yh.nackademin.se>
2020-10-26 10:55:53 +0100

    skapat en metod som beräknar avståndet mellan två punkter

--- src/Point.java
+++ src/Point.java
@@ -5,4 +5,18 @@ public class Point {
     double x; // x-koordinat
     double y; // y-koordinat
 
+
+    /**
+     *  en klassmetod som beräknar avståndet mellan två punkter
+     * @param p1 är en referensvariabel till den första punkten
+     * @param p2 är en referensvariabel till den andra punkten
+     * @return avståndet mellan punkterna
+     */
+    public static double distance (Point p1, Point p2){
+
+        //beräkna avståndet
+        return Math.sqrt( Math.pow(p2.x - p1.x, 2) + (p2.y - p1.y) * (p2.y - p1.y));
+
+    }
+
 }
--- src/PointDemo.java
+++ src/PointDemo.java
@@ -18,10 +18,22 @@ public class PointDemo {
         p1.y = -5; // flyttar y 5 steg neråt
 
         // flytta punkt 2 (p2)
-        p2.x = 10.5;
-        p2.y = -5.5;
+        //p2.x = 10.5;
+        //p2.y = -5.5;
 
         System.out.println("p1: (" + p1.x + " , " + p1.y + ")" );
         System.out.println("p2: (" + p2.x + " , " + p2.y + ")" );
+
+        //beräkna avståndet mellan p1 och p2
+
+       double d = Point.distance(p1, p2);
+        System.out.printf("avstånd = %.2f", d);
+
+        // flytta punkt 1
+        p1.x = 10;
+        p1.y = 0;
+
+        System.out.printf("\navstånd = %.2f" , Point.distance(p1, p2));
+
     }
 }
